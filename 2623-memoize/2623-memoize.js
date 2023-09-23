4288dc58d48ceef99560dc04a1bcdc83b470236b
Nupni <37250284+Nupni@users.noreply.github.com>
2023-09-23 22:21:24 +0700

    Time: 328 ms (22.77%), Space: 107.3 MB (65.96%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const memo = {}
+    return function(...args) {
+        if(`${args}`in memo) {
+            return memo[`${args}`]
+        } else {
+            return memo[`${args}`] = fn(...args)
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
