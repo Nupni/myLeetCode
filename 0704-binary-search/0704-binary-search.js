8ec029592b39c919f686b59c6ac01113ce3a141d
Nupni <37250284+Nupni@users.noreply.github.com>
2023-09-25 02:46:50 +0700

    Time: 54 ms (65.23%), Space: 44.1 MB (98.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    // return nums.indexOf(target)
+    const start = 0
+    const end = nums.length - 1
+    const findNum = (target, start, end) => {
+        if(start > end) return -1
+
+        const mid = Math.floor((start + end) / 2)
+
+        if(nums[mid] === target) return mid
+
+        if(nums[mid] > target) return findNum(target, start, mid - 1)
+
+        if(nums[mid] < target) return findNum(target, mid + 1, end)
+    }
+
+    return findNum(target, start, end)
 };
