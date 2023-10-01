a2ae8fc6d7d9cea8f494323deebc996d99b18187
Nupni <37250284+Nupni@users.noreply.github.com>
2023-10-02 04:37:41 +0700

    Time: 67 ms (93.22%), Space: 52.8 MB (89.67%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let check = {};
+    for(let i = 0; i < nums.length - 1; i++) {
+        if(nums[i] > nums[i + 1]) {
+            check["-"] = 1
+        } else if(nums[i] < nums[i + 1]) {
+            check["+"] = 1
+        }
+    }
+    const result = Object.keys(check).length === 2 ? false : true
+    return result
+};
