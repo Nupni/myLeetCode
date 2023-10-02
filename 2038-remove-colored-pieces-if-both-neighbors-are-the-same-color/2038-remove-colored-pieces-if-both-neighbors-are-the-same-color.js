8668d0c7693e63d9ea551796b55880a5e3a8cc6b
Nupni <37250284+Nupni@users.noreply.github.com>
2023-10-02 22:52:17 +0700

    Time: 83 ms (25.00%), Space: 46.2 MB (28.33%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function(colors) {
+  const n = colors.length;
+  let Awin = 0, Bwin = 0;
+
+  const checkColor = (i, text) => {
+    const c1 = colors[i];
+    const c2 = colors[i + 1];
+    const c3 = colors[i + 2];
+
+    return c1 === text && c2 === text && c3 === text
+  }
+ 
+  for (let i = 0; i < n - 2; i++) {
+    if (checkColor(i, "A")) Awin++
+    else if (checkColor(i, "B")) Bwin++
+  }
+
+  if (Awin > Bwin) return true;
+  return false;
+};
