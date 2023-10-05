0dd61dcfbd8759e8720222ada79eeedd0b1d828b
Nupni <37250284+Nupni@users.noreply.github.com>
2023-10-05 20:41:38 +0700

    Time: 60 ms (54.60%), Space: 43.8 MB (49.05%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+    let result = []
+    const memo = {}
+    for(let i = 0; i < nums.length; i++) {
+        let v = nums[i]
+
+        if(!memo[v]) {
+            memo[v] = 1
+        } else {
+            memo[v] += 1
+        }
+    }
+
+     for(let key in memo) {
+        if(memo[key] > (nums.length) / 3) {
+            result.push(key) 
+        }
+    }
+
+    return result
+};
